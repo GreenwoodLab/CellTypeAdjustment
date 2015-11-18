3ab80e49850805ef75a0080838da3f7693caf5eb
Kevin McGregor <kevin.mcgregor@mail.mcgill.ca>
2015-11-18 10:37:11 -0500

    made code to output data for use in EWASher/CCD

--- ct_adjustment_example.R
+++ ct_adjustment_example.R
@@ -38,6 +38,7 @@ p.ref = lm.ref$p.value[,2]
 
 ###################################################
 #Reference-free method
+library(RefFreeEWAS)
 
 #Estimated latent dimension
 est_dim = EstDimRMT(t(scale(t(sim_beta - lm.unadj$coef %*% t(X.unadj)))), plot=FALSE)$dim
@@ -66,7 +67,7 @@ SVA = sva(sim_beta, model_mat, null_model_mat, method="irw")
 #Calculate p-values
 update_model = cbind(model_mat, SVA$sv)
 update_null_model = cbind(null_model_mat, SVA$sv)
-p_sva = f.pvalue(sim_beta, update_model, update_null_model)
+p.sva = f.pvalue(sim_beta, update_model, update_null_model)
 
 ###################################################
 #Independent Surrogate Variable Analysis
@@ -102,3 +103,36 @@ ctl = rep(FALSE, dim(sim_beta)[1]); ctl[subset_controls] = TRUE
 ruvK = getK(t(sim_beta), matrix(disease_status), ctl)$k
 ruv_mod = RUV4(t(sim_beta), matrix(disease_status), ctl, k=ruvK)
 p.ruv = ruv_mod$p
+
+##################################################
+#EWASher and CellCDecon need to be run externally... See scripts for these two methods
+
+#Function to save data to text files for use in EWASher and CellCDecon 
+#Function from http://research.microsoft.com/en-us/downloads/472fe637-7cb9-47d4-a0df-37118760ccd1/
+makefiles_FLE = function(rSrcDir, data, phen, covar)
+{        
+  #write each input to file, then call RunFLEFromFiles
+  write.table(data, file = "datafileTmp.txt", col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
+  write.table(phen, file = "phenfileTmp.txt", col.names = FALSE, row.names = FALSE, sep = "\t", quote = FALSE)
+  write.table(covar, file = "covarfileTmp.txt", col.names = FALSE, row.names = FALSE, sep = "\t", quote = FALSE)
+}
+
+#Need to manipulate beta matrix to what EWASher and CCD expect
+tmp_beta = data.frame(rownames(sim_beta), sim_beta)
+colnames(tmp_beta) = c("ID", colnames(sim_beta))
+
+#Directory to output files to
+path_to_dir = "/home/data1/homeldi/kevin.mcgregor/research/marie_hudson/r_scripts/mixtures/github_tutorial"
+
+#Make the EWASher input files
+makefiles_FLE(path_to_dir, tmp_beta, data.frame(colnames(tmp_beta)[-1], disease_status), covar=NULL) 
+
+#Or save another version with patients' true disease (coded) from original 450K data before simulation
+#as a covariate to compare performance of EWASher
+makefiles_FLE(path_to_dir, tmp_beta, 
+              data.frame(colnames(tmp_beta)[-1], disease_status),
+              data.frame(1, true_disease))  
+
+
+
+
